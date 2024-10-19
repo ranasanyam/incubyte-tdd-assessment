ddf322281d375b814a12e45bbdc3daf27b85fe0c
ranasanyam <ranasanyam8445@gmail.com>
2024-10-19 20:50:49 +0530

    handle one and two numbers

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,8 @@
 function add(numbers) {
-    return numbers === '' ? 0 : parseInt(numbers);
+    // return numbers === '' ? 0 : parseInt(numbers);
+    if(numbers === '') return 0;
+    const numArray = numbers.split(',');
+    return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
 module.exports = {add};
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,5 +1,12 @@
 const { add } = require('../src/calculator');
 
-test('should return 0 for an empty string', () => {
-    expect(add('')).toBe(0);
+// test('should return 0 for an empty string', () => {
+//     expect(add('')).toBe(0);
+// });
+
+test('should return the number itself if only one number is provided', () => {
+    expect(add('1')).toBe(1);
+});
+test('should return the sum of two numbers separated by a comma', () => {
+    expect(add('1,5')).toBe(6);
 });
