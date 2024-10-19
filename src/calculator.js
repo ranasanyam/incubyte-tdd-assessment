8d2e8d1bf38b796887fc25136b6ffa37cdba4a3e
ranasanyam <ranasanyam8445@gmail.com>
2024-10-19 20:54:01 +0530

    support newlines

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,10 @@
 function add(numbers) {
     // return numbers === '' ? 0 : parseInt(numbers);
     if(numbers === '') return 0;
-    const numArray = numbers.split(',');
+    // const numArray = numbers.split(',');
+    // return numArray.reduce((sum, num) => sum + parseInt(num), 0);
+
+    const numArray = numbers.split(/[\n,]/);
     return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -4,9 +4,13 @@ const { add } = require('../src/calculator');
 //     expect(add('')).toBe(0);
 // });
 
-test('should return the number itself if only one number is provided', () => {
-    expect(add('1')).toBe(1);
-});
-test('should return the sum of two numbers separated by a comma', () => {
-    expect(add('1,5')).toBe(6);
+// test('should return the number itself if only one number is provided', () => {
+//     expect(add('1')).toBe(1);
+// });
+// test('should return the sum of two numbers separated by a comma', () => {
+//     expect(add('1,5')).toBe(6);
+// });
+
+test('should return the sum with newlines between numbers', () => {
+    expect(add('1\n2,3')).toBe(6);
 });
